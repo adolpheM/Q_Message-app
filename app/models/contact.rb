906a0fddcab63fc85c68cfa1e8a23e246aab10b4
adolpheM <adojoe25@gmail.com>
2017-03-30 12:44:31 +0200

    Designed the app

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,4 +4,7 @@ class Contact < ApplicationRecord
   has_many :contact_groups
   has_many :groups, through: :contact_groups
 
+  validates :name, presence: true
+  validates :phone_number, presence: true
+
 end
